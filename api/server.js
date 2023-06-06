9a6e063c8728b22df6002a8a0d41629f0ff88b35
Lucas <lucaslemoine49@gmail.com>
2023-06-06 14:46:23 +0100

    completed frontend with styling

--- api/server.js
+++ api/server.js
@@ -1,49 +1,63 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-mongoose.connect("mongodb://127.0.0.1:27017/mern-todo", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log("connected to db"))
-    .catch(console.log("error"))
+mongoose.connect('mongodb://127.0.0.1:27017/react-todo', {
+	useNewUrlParser: true, 
+	useUnifiedTopology: true 
+}).then(() => console.log("Connected to MongoDB")).catch(console.error);
 
-const Todo = require('./models/todo')
+// Models
+const Todo = require('./models/Todo');
 
-app.get('/todos', async(req, res) => {
-    const todos = await Todo.find()
+app.get('/todos', async (req, res) => {
+	const todos = await Todo.find();
+	res.json(todos);
+});
 
-    res.json(todos)
-})
+app.post('/todo/new', async (req, res) => {
+	const todo = new Todo({
+		text: req.body.text
+	})
 
-app.post('/todo/new', async(req, res) => {
-    const todo = new Todo({
-        text: req.body.text
-    })
+	await todo.save();
 
-    todo.save()
-    res.json(todo)
-})
+	res.json(todo);
+});
 
 app.delete('/todo/delete/:id', async (req, res) => {
-    const result = await Todo.findByIdAndDelete(req.params.id)
+	await Todo.findByIdAndDelete(req.params.id);
+	res.json({ success: true });
+});
 
-    res.json(result)
-})
+app.get('/todo/complete/:id', async (req, res) => {
+    try {
+      const todo = await Todo.findById(req.params.id);
+  
+      if (!todo) {
+        return res.status(404).json({ error: 'Todo not found' });
+      }
+  
+      todo.complete = !todo.complete;
+      await todo.save();
+  
+      res.json(todo);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Server error' });
+    }
+});
 
-app.put('/todo/complete/:id', async (req, res) => {
-    const todo = await Todo.findById(req.params.id)
+app.put('/todo/update/:id', async (req, res) => {
+	const todo = await Todo.findById(req.params.id);
+	todo.text = req.body.text;
+	await todo.save();
+	res.json(todo);
+});
 
-    todo.complete = !todo.complete
-
-    todo.save()
-    res.json(todo)
-})
-
-app.listen(3001, () => console.log("server started on port 3001"))
+app.listen(3001);
--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,121 @@
+import { useEffect, useState } from 'react';
+import './index.css';
+const api_base = 'http://localhost:3001';
+
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  useEffect(() => {
+    GetTodos();
+  }, []);
+
+  const GetTodos = () => {
+    fetch(api_base + '/todos')
+      .then((res) => res.json())
+      .then((data) => setTodos(data))
+      .catch((err) => console.error('Error: ', err));
+  };
+
+  const completeTodo = async (id) => {
+    const data = await fetch(api_base + '/todo/complete/' + id).then((res) => res.json());
+
+    setTodos((todos) =>
+      todos.map((todo) => {
+        if (todo._id === data._id) {
+          return { ...todo, complete: data.complete };
+        }
+        return todo;
+      })
+    );
+  };
+
+  const addTodo = async (e) => {
+    e.preventDefault();
+    if (newTodo.trim() === '') return;
+    const data = await fetch(api_base + '/todo/new', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        text: newTodo,
+      }),
+    }).then((res) => res.json());
+
+    setTodos([data, ...todos]);
+    setNewTodo('');
+  };
+
+  const deleteTodo = async (id) => {
+    const response = await fetch(api_base + '/todo/delete/' + id, { method: 'DELETE' });
+    if (response.ok) {
+      setTodos((todos) => todos.filter((todo) => todo._id !== id));
+    } else {
+      console.error('Error deleting todo');
+    }
+  };
+
+  const todoItems = todos.filter((todo) => !todo.complete);
+  const completedItems = todos.filter((todo) => todo.complete);
+
   return (
     <div className="App">
-      <h1> welcome </h1>
-      <h2> test </h2>
-      <div className="todos">
-				<div className="todo">
-					<div className="checkbox"></div>
+      <div className="title-container">
+        <h1>Todo</h1>
+        <h4>Your tasks</h4>
+      </div>
+      
+      <div className="content-container">
+        <h5>Todo</h5>
+        
+        <div className="todos">
+          {todoItems.length > 0 ? (
+            todoItems.map((todo) => (
+              <div
+                className={'todo' + (todo.complete ? ' is-complete' : '')}
+                key={todo._id}
+                onClick={() => completeTodo(todo._id)}
+              >
+                <div className="checkbox" style={todo.complete ? { backgroundImage: "url('/tick.png')" } : {}}></div>
+                <div className="text">{todo.text}</div>
+                <div className="delete-todo" onClick={(e) => { e.stopPropagation(); deleteTodo(todo._id); }} style={{ backgroundImage: "url('/bin.png')" }}>
+                </div>
+              </div>
+            ))
+          ) : (
+            <p> </p>
+          )}
 
-          <div className="text"> task1 </div>
+          <h5>Completed</h5>
+          {completedItems.length > 0 ? (
+            completedItems.map((todo) => (
+              <div
+                className={'todo' + (todo.complete ? ' is-complete' : '')}
+                key={todo._id}
+                onClick={() => completeTodo(todo._id)}
+              >
+                <div className="checkbox" style={todo.complete ? { backgroundImage: "url('/tick.png')" } : {}}></div>
+                <div className="text">{todo.text}</div>
+                <div className="delete-todo" onClick={(e) => { e.stopPropagation(); deleteTodo(todo._id); }} style={{ backgroundImage: "url('/bin.png')" }}>
+                </div>
+              </div>
+            ))
+          ) : (
+            <p> </p>
+          )}
+        </div>
 
-          <div className="delete-todo">x</div>
-			  </div>
-
-        <div className="todo is-complete">
-          <div className="checkbox"></div>
-
-          <div className="text"> task2 </div>
-
-          <div className="delete-todo">x</div>
+        <div className="input-form-container">
+          <form onSubmit={addTodo} className="input-form">
+            <input
+              type="text"
+              className="add-todo-input"
+              placeholder="Add a new task..."
+              value={newTodo}
+              onChange={(e) => setNewTodo(e.target.value)}
+            />
+          </form>
         </div>
       </div>
     </div>
